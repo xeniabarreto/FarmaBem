5b5cc7ed98976bc4e34fa673285e3702bf68ac38
Xênia Barreto <xeniabarreto22@gmail.com>
2021-12-12 20:25:11 -0300

    update Medicine Schema and CheckList

--- src/models/medicineSchema.js
+++ src/models/medicineSchema.js
@@ -8,12 +8,6 @@ const medicineSchema = new mongoose.Schema ({
         required: true
     },
 
-    stokMedicine: {
-        type: Number,
-        required: false,
-        default: 0
-    },
-
     medicine_name:{
         type: String,
         required: true

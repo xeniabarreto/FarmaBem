5fd23a8de2caf7fa08f62d7a90e6deb465ef6d17
Xênia Barreto <xeniabarreto22@gmail.com>
2021-12-12 23:54:25 -0300

    update message Index

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,8 @@ router.get("/", (req, res) =>{
     res.status(200).json({
         "titulo": "API Farma Bem",
         "version":"1.0.0",
-        "mensagem":"Criada com proposito de doações de medicamentos."
+        "mensagem":"Criada com proposito de doações de medicamentos.",
+        "mensagem":"Seja bem-vinde :)"
     })
 })
 

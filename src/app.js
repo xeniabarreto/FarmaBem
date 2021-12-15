23c3c406e242fe5889d541f34e49e0387bc8b1ea
Xênia Barreto <xeniabarreto22@gmail.com>
2021-12-15 14:53:53 -0300

    update redme

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger/swagger_output.json');
 
@@ -18,7 +18,7 @@ db.connect()
 
 app.use(cors());
 app.use(express.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.use("/", index);

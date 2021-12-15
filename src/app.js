def844d41863e1da3a716590831db6575f234ce0
Xênia Barreto <xeniabarreto22@gmail.com>
2021-12-15 14:48:10 -0300

    config swagger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+//const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger/swagger_output.json');
 
 require('dotenv-safe').config(); 
 const db = require('./database/mongoConfig');
-
 
 const app = express();
 
@@ -16,11 +18,14 @@ db.connect()
 
 app.use(cors());
 app.use(express.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.use("/", index);
 app.use("/pharmacy", phamarcyRoutes);
 app.use("/medicine", medicineRoutes);
 app.use("/adm", amdRoutes);
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 module.exports = app

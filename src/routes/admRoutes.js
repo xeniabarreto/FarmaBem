0a439172adb6d930ee952b5cf36696c22e996dd4
Xênia Barreto <xeniabarreto22@gmail.com>
2021-12-15 22:18:18 -0300

    update check-list

--- src/routes/admRoutes.js
+++ src/routes/admRoutes.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const loginAuth = require('../helpers/auth');
+
 const { checkToken } = require("../middlewares/auth");
 
 const controller = require ('../controllers/admController')
 
 router.get("/all", checkToken, controller.getAll); //rota privada
-router.get("/id/:id", controller.getById);
-router.post("/register", controller.register);
-router.post("/login", controller.loginWithCreatingToken);
+router.get("/id/:id", checkToken, controller.getById);
+router.post("/register", controller.register); // publica
+router.post("/login", controller.loginWithCreatingToken); // publica
 router.put("/update/:id", controller.updateById);
 router.delete("/delete/:id", controller.deleteById);
+
+
 
 
 module.exports = router
